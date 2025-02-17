e912c1ce302991575d69a2f59d52744d93b14c2a
gh4ni404 <gania828@gmail.com>
2025-02-17 14:13:45 +0800

    UPDATE: integrate SweetAlert2 for improved user feedback and enhance form submission handling in script-absen.js and absen-management.html

--- data/script-absen.js
+++ data/script-absen.js
@@ -1,6 +1,32 @@
 const url = 'https://script.google.com/macros/s/AKfycbwwr-VYZQKHK8oWFOGydcbegugGoYXQIaDgnxyAmgF_CMk2hbEM7S7Q-xofCPM-ryJ7/exec';
 const userRole = localStorage.getItem('userRole');
 const user = localStorage.getItem('userUser');
+const absenForm = document.getElementById("absenForm");
+const submitBtn = absenForm.querySelector("button[type='submit']");
+const inputs = absenForm.querySelectorAll("input, select");
+let emptyField = null;
+for (let input of inputs) {
+  if (input.value.trim() === "") {
+    emptyField = input;
+    break;
+  }
+}
+const Swal2 = Swal.mixin({
+  customClass: {
+    input: 'form-control'
+  }
+});
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+});
 let siswaListData = []; // Menyimpan daftar siswa agar tidak perlu fetch ulang saat submit
 
 async function loadGuruData() {
@@ -33,19 +59,19 @@ async function loadSiswaByKelas(kelas) {
       <td class="text-center">${index + 1}</td>
       <td class="w-md-50">${siswa.nama_siswa}</td>
       <td class="text-md-center">
-        <div class="form-check form-check-success form-check-inline">
+        <div class="form-check form-check-inline">
           <input class="form-check-input" type="radio" name="status_${siswa.id}" id="hadir_${siswa.id}" value="Hadir" checked>
           <label class="form-check-label" for="hadir_${siswa.id}">Hadir</label>
         </div>
-        <div class="form-check form-check-warning form-check-inline">
+        <div class="form-check form-check-inline">
           <input class="form-check-input" type="radio" name="status_${siswa.id}" id="izin_${siswa.id}" value="Izin">
           <label class="form-check-label" for="izin_${siswa.id}">Izin</label>
         </div>
-        <div class="form-check form-check-info form-check-inline">
+        <div class="form-check form-check-inline">
           <input class="form-check-input" type="radio" name="status_${siswa.id}" id="sakit_${siswa.id}" value="Sakit">
           <label class="form-check-label" for="sakit_${siswa.id}">Sakit</label>
         </div>
-        <div class="form-check form-check-danger form-check-inline">
+        <div class="form-check form-check-inline">
           <input class="form-check-input" type="radio" name="status_${siswa.id}" id="alpa_${siswa.id}" value="Alpa">
           <label class="form-check-label" for="alpa_${siswa.id}">Alpa</label>
         </div>
@@ -57,7 +83,7 @@ async function loadSiswaByKelas(kelas) {
   `).join('');
 }
 
-document.getElementById('absenForm').addEventListener('submit', async function (e) {
+absenForm.addEventListener('submit', async function (e) {
   e.preventDefault();
   const mapel = document.getElementById('mapel').value;
   const kelas = document.getElementById('kelas').value;
@@ -65,9 +91,15 @@ document.getElementById('absenForm').addEventListener('submit', async function (
   const user_guru = user;
 
   if (!tanggal) {
-    alert("Silakan pilih tanggal terlebih dahulu.");
+    
+    
     return;
   }
+
+  submitBtn.innerHTML = `
+  <span class="spinner-border spinner-border-sm"></span> Menyimpan...
+  `;
+  submitBtn.disabled = true;
 
   const absensiData = siswaListData.map(siswa => ({
     id_siswa: siswa.id,
@@ -82,17 +114,31 @@ document.getElementById('absenForm').addEventListener('submit', async function (
   console.log('Data yang dikirim:', absensiData);
 
   // Kirim data absensi ke backend
-  const response = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({
-      action: 'addAbsen',
-      absensiData
-    })
-  });
-
-  const result = await response.json();
-  console.log('Hasilnya:', result);
-  alert(result.message);
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        action: 'addAbsen',
+        absensiData
+      })
+    });
+    if (response.ok) {
+      const result = await response.json();
+      console.log('Hasilnya:', result);
+      Swal.fire({
+        icon: "success",
+        title: "Berhasil",
+        text: result.message
+      });
+    } else {
+      throw new Error("Gagal Menyimpan Absensi. Coba lagi.");
+    }
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    submitBtn.innerHTML = "Simpan";
+    submitBtn.disabled = false;
+  }
 });
 
 loadGuruData();
