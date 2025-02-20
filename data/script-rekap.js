56e70a9216e36769891cc3ad5f66ec89a25ed009
gh4ni404 <gania828@gmail.com>
2025-02-21 00:37:41 +0800

    UPDATE: add chart for class attendance recap and implement data fetching for user-specific charts in index.html and script-rekap.js

--- data/script-rekap.js
+++ data/script-rekap.js
@@ -1,5 +1,5 @@
 const url = 'https://script.google.com/macros/s/AKfycbwwr-VYZQKHK8oWFOGydcbegugGoYXQIaDgnxyAmgF_CMk2hbEM7S7Q-xofCPM-ryJ7/exec';
-
+const user = localStorage.getItem('userUser');
 async function fetchDashData() {
   try {
     let response = await fetch(`${url}?action=getDashboardData`);
@@ -31,12 +31,90 @@ async function loadChart() {
     dataLabels: { enabled: false },
     xaxis: {
       categories: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Des"],
-
     },
   };
   const chart = new ApexCharts(document.querySelector("#chart-profile-visit"), options);
   chart.render();
 }
 
+async function loadChartKelas(userGuru) {
+  const response = await fetch(`${url}?action=getRekapByGuru&user_guru=${userGuru}`);
+  const data = await response.json();
+  if (!data) return;
+
+  const container = document.getElementById('container-charts');
+  const title_container = document.getElementById('names-charts');
+  const count_container = document.getElementById('count-charts');
+
+  Object.keys(data).forEach(kelas => {
+    const kelasData = data[kelas];
+    let totalData = 0;
+    const chartDiv = document.createElement("div");
+    chartDiv.className = "row";
+    
+    const categories = Object.keys(kelasData);
+    const seriesData = {
+      hadir: [],
+      sakit: [],
+      izin: [],
+      alpa: [],
+    };
+    categories.forEach(mapel => {
+      const rekapan = kelasData[mapel];
+      seriesData.hadir.push(rekapan.Hadir);
+      seriesData.sakit.push(rekapan.Sakit);
+      seriesData.izin.push(rekapan.Izin);
+      seriesData.alpa.push(rekapan.Alpa);
+      totalData += (rekapan.Hadir || 0) + (rekapan.Sakit || 0) + (rekapan.Izin || 0) + (rekapan.Alpa || 0);
+    });
+
+    chartDiv.innerHTML = `
+                      <div class="col-10">
+                        <div class="d-flex align-items-center">
+                          <svg class="bi text-primary" width="32" height="32" fill="blue" style="width: 10px;">
+                            <use xlink:href="/assets/static/images/bootstrap-icons.svg#circle-fill"/>
+                          </svg>
+                          <h6 class="mb-0 ms-3">${kelas}</h6>
+                        </div>
+                      </div>
+                      <div class="col-2">
+                        <h6 class="mb-0 text-end">${totalData}</h6>
+                      </div>
+                      <div class="col-12">
+                        <div id="chart-${kelas}"></div>
+                      </div>
+    `;
+    container.appendChild(chartDiv);
+    const options = {
+      series: [
+        { name: "Hadir", data: seriesData.hadir },
+        { name: "Sakit", data: seriesData.sakit },
+        { name: "Izin", data: seriesData.izin },
+        { name: "Alpa", data: seriesData.alpa },
+      ],
+      chart: {
+        height: 80,
+        type: "area",
+        toolbar: { show: false }
+      },
+      stroke: { width: 2 },
+      grid: { show: false },
+      dataLabels: { enabled: false },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        axisBorder: { show: false },
+        axisTicks: { show: false },
+        labels: { show: false },
+      },
+      show: false,
+      yaxis: { labels: { show: false } },
+      legend: { show: false }
+    };
+    const chart = new ApexCharts(chartDiv, options);
+    chart.render();
+  });
+}
+
 fetchDashData();
 loadChart();
+loadChartKelas(user);
